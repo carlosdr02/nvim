439f8c3c7f91a54b7d4108273bb3c8b5ded00708
carlosdr02 <carlosdelgadoramos2002@gmail.com>
2023-07-26 09:53:04 +0200

    Minor reordering

--- init.lua
+++ init.lua
@@ -1,3 +1,25 @@
+-- Keymaps
+vim.g.mapleader = ' '
+
+vim.keymap.set('i', 'jk', '<esc>')
+vim.keymap.set('i', 'jK', '<esc>')
+vim.keymap.set('i', 'Jk', '<esc>')
+vim.keymap.set('i', 'JK', '<esc>')
+
+vim.keymap.set('i', 'kj', '<esc>')
+vim.keymap.set('i', 'kJ', '<esc>')
+vim.keymap.set('i', 'Kj', '<esc>')
+vim.keymap.set('i', 'KJ', '<esc>')
+
+vim.keymap.set('n', '<leader>w', '<cmd>w<cr>')
+
+vim.keymap.set('n', '<c-h>', '<c-w>h')
+vim.keymap.set('n', '<c-j>', '<c-w>j')
+vim.keymap.set('n', '<c-k>', '<c-w>k')
+vim.keymap.set('n', '<c-l>', '<c-w>l')
+vim.keymap.set('n', '<m-q>', '<cmd>q!<cr>')
+vim.keymap.set('n', '<m-o>', '<cmd>on<cr>')
+
 -- Editor settings
 vim.o.number = true
 vim.o.relativenumber = true
@@ -28,9 +50,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
-
--- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.g.mapleader = ' '
 
 require('lazy').setup({
     {
@@ -63,28 +82,7 @@ require('lazy').setup({
     'L3MON4D3/LuaSnip'
 })
 
-vim.cmd.colorscheme('tokyonight')
-
--- Keymaps
-vim.keymap.set('i', 'jk', '<esc>')
-vim.keymap.set('i', 'jK', '<esc>')
-vim.keymap.set('i', 'Jk', '<esc>')
-vim.keymap.set('i', 'JK', '<esc>')
-
-vim.keymap.set('i', 'kj', '<esc>')
-vim.keymap.set('i', 'kJ', '<esc>')
-vim.keymap.set('i', 'Kj', '<esc>')
-vim.keymap.set('i', 'KJ', '<esc>')
-
-vim.keymap.set('n', '<leader>w', '<cmd>w<cr>')
-
-vim.keymap.set('n', '<c-h>', '<c-w>h')
-vim.keymap.set('n', '<c-j>', '<c-w>j')
-vim.keymap.set('n', '<c-k>', '<c-w>k')
-vim.keymap.set('n', '<c-l>', '<c-w>l')
-vim.keymap.set('n', '<m-q>', '<cmd>q!<cr>')
-vim.keymap.set('n', '<m-o>', '<cmd>on<cr>')
-
+-- Plugins keymaps
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -94,6 +92,9 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 --vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '<c-p>', vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<c-n>', vim.diagnostic.goto_next)
+
+-- Plugins editor settings
+vim.cmd.colorscheme('tokyonight')
 
 -- Plugins config
 local actions = require('telescope.actions')
