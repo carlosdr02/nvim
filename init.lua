6346cf76b5747ea95f4e3cf50a232a6c098fd4aa
carlosdr02 <carlosdelgadoramos2002@gmail.com>
2023-08-14 11:47:54 +0200

    Removed the <leader>fs command for lsp document symbols

--- init.lua
+++ init.lua
@@ -63,7 +63,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
         vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', builtin.lsp_references, opts)
-        vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, opts)
     end
 })
 
