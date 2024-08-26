12886991e14ac0c6063112e879a7ce97eeb56a93
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2024-08-26 02:11:01 +0200

    Added harpoon

--- init.lua
+++ init.lua
@@ -96,6 +96,12 @@ require('lazy').setup({
         end
     },
 
+    -- Harpoon
+    {
+        'theprimeagen/harpoon',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+
     -- Tmux
     {
         "aserowy/tmux.nvim",
@@ -276,3 +282,19 @@ vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeFindFileToggle<cr>')
 
 vim.keymap.set('n', '<leader>v', '<cmd>vs<cr>')
 vim.keymap.set('n', '<leader>h', '<cmd>sp<cr>')
+
+local mark = require('harpoon.mark')
+local ui = require('harpoon.ui')
+
+vim.keymap.set('n', '<leader>a', mark.add_file)
+vim.keymap.set('n', '<leader>m', ui.toggle_quick_menu)
+
+vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
+vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
+vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end)
+vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end)
+vim.keymap.set('n', '<leader>5', function() ui.nav_file(5) end)
+vim.keymap.set('n', '<leader>6', function() ui.nav_file(6) end)
+vim.keymap.set('n', '<leader>7', function() ui.nav_file(7) end)
+vim.keymap.set('n', '<leader>8', function() ui.nav_file(8) end)
+vim.keymap.set('n', '<leader>9', function() ui.nav_file(9) end)
