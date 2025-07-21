3e3d213245b07a1b922ca874b5dcb7139d7b5ea1
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2025-07-21 14:44:56 +0200

    added treesitter text objects

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -59,9 +59,29 @@ require('lazy').setup({
                     highlight = {
                         enable = true
                     },
+                    textobjects = {
+                        select = {
+                            enable = true,
+                            lookahead = true,
+                            keymaps = {
+                                ["af"] = "@function.outer",
+                                ["if"] = "@function.inner",
+                                ["ac"] = "@class.outer",
+                                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                                ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+                            },
+                            selection_modes = {
+                                ['@parameter.outer'] = 'v',
+                                ['@function.outer'] = 'V',
+                                ['@class.outer'] = '<c-v>',
+                            },
+                            include_surrounding_whitespace = true,
+                        },
+                    },
                 })
             end
         },
+        'nvim-treesitter/nvim-treesitter-textobjects',
         {
             'mason-org/mason-lspconfig.nvim',
             opts = {},
