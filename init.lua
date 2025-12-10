fdb7a4f7f5656c721becfa0d629c66e66f5485ed
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2025-12-10 16:19:28 +0100

    added lazydev

--- init.lua
+++ init.lua
@@ -88,7 +88,15 @@ require("lazy").setup({
                 -- Default list of enabled providers defined so that you can extend it
                 -- elsewhere in your config, without redefining it, due to `opts_extend`
                 sources = {
-                    default = { 'lsp', 'path', 'snippets', 'buffer' },
+                    default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+                    providers = {
+                        lazydev = {
+                            name = "LazyDev",
+                            module = "lazydev.integrations.blink",
+                            -- make lazydev completions top priority (see `:h blink.cmp`)
+                            score_offset = 100,
+                        },
+                    }
                 },
 
                 -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
@@ -99,6 +107,17 @@ require("lazy").setup({
                 fuzzy = { implementation = "prefer_rust_with_warning" }
             },
             opts_extend = { "sources.default" }
+        },
+        {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    -- See the configuration section for more details
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
         }
     },
     -- Configure any other settings here. See the documentation for more details.
