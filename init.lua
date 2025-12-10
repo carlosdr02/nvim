18304861de801c69ac71de4d8025b784f33ca2fa
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2025-12-10 17:57:03 +0100

    more options

--- init.lua
+++ init.lua
@@ -31,6 +31,16 @@ vim.opt.shiftround = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.diagnostic.config({
+    virtual_text = true
+})
 
 -- Setup lazy.nvim
 require("lazy").setup({
