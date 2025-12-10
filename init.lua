e898085e3e3500adea5f419d8fa3b28bdf5ddb03
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2025-12-10 16:02:26 +0100

    added lua_ls

--- init.lua
+++ init.lua
@@ -39,7 +39,9 @@ require("lazy").setup({
         },
         {
             "mason-org/mason-lspconfig.nvim",
-            opts = {},
+            opts = {
+                ensure_installed = { 'lua_ls' }
+            },
             dependencies = {
                 { "mason-org/mason.nvim", opts = {} },
                 "neovim/nvim-lspconfig",
