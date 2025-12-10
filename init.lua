88b0e19b72ab344f25172507ab93d8d0c6e19e28
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2025-12-10 15:49:54 +0100

    initial

--- init.lua
+++ init.lua
@@ -1,3 +1,47 @@
-require('options')
-require('plugins')
-require('keymaps')
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.signcolumn = 'no'
+
+-- Setup lazy.nvim
+require("lazy").setup({
+    spec = {
+        {
+            "rose-pine/neovim", name = "rose-pine",
+            config = function()
+                vim.cmd.colorscheme('rose-pine')
+            end
+        },
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    -- @carlosdr02   changed this to false
+    checker = { enabled = false },
+})
