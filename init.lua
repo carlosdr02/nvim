bb444b609f0712d1719fdc78c21a7bfad788b028
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2025-12-11 16:52:43 +0100

    removed comments

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -15,11 +14,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = " "
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -45,7 +41,6 @@ vim.diagnostic.config({
     virtual_text = true
 })
 
--- Setup lazy.nvim
 require("lazy").setup({
     spec = {
         {
@@ -72,68 +67,25 @@ require("lazy").setup({
         },
         {
             'saghen/blink.cmp',
-            -- optional: provides snippets for the snippet source
             dependencies = { 'rafamadriz/friendly-snippets' },
-
-            -- use a release tag to download pre-built binaries
             version = '1.*',
-            -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-            -- build = 'cargo build --release',
-            -- If you use nix, you can build from source using latest nightly rust with:
-            -- build = 'nix run .#build-plugin',
-
-            ---@module 'blink.cmp'
-            ---@type blink.cmp.Config
             opts = {
-                -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-                -- 'super-tab' for mappings similar to vscode (tab to accept)
-                -- 'enter' for enter to accept
-                -- 'none' for no mappings
-                --
-                -- All presets have the following mappings:
-                -- C-space: Open menu or open docs if already open
-                -- C-n/C-p or Up/Down: Select next/previous item
-                -- C-e: Hide menu
-                -- C-k: Toggle signature help (if signature.enabled = true)
-                --
-                -- See :h blink-cmp-config-keymap for defining your own keymap
                 keymap = { preset = 'default' },
-
                 appearance = {
-                    -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-                    -- Adjusts spacing to ensure icons are aligned
                     nerd_font_variant = 'mono'
                 },
-
-                -- (Default) Only show the documentation popup when manually triggered
                 completion = { documentation = { auto_show = false } },
-
-                -- Default list of enabled providers defined so that you can extend it
-                -- elsewhere in your config, without redefining it, due to `opts_extend`
                 sources = {
                     default = { 'lsp', 'path', 'snippets', 'buffer' }
                 },
-
-                -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-                -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-                -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-                --
-                -- See the fuzzy documentation for more information
                 fuzzy = { implementation = "prefer_rust_with_warning" }
             },
             opts_extend = { "sources.default" }
         },
         {
             "ibhagwan/fzf-lua",
-            -- optional for icon support
             dependencies = { "nvim-tree/nvim-web-devicons" },
-            -- or if using mini.icons/mini.nvim
-            -- dependencies = { "nvim-mini/mini.icons" },
-            ---@module "fzf-lua"
-            ---@type fzf-lua.Config|{}
-            ---@diagnostics disable: missing-fields
             opts = {}
-            ---@diagnostics enable: missing-fields
         },
         {
             'nvim-lualine/lualine.nvim',
@@ -151,13 +103,8 @@ require("lazy").setup({
         },
         {
             'stevearc/oil.nvim',
-            ---@module 'oil'
-            ---@type oil.SetupOpts
             opts = {},
-            -- Optional dependencies
             dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-            -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-            -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
             lazy = false,
         },
         {
@@ -173,11 +120,7 @@ require("lazy").setup({
             end
         }
     },
-    -- Configure any other settings here. See the documentation for more details.
-    -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "habamax" } },
-    -- automatically check for plugin updates
-    -- @carlosdr02 changed this to false
     checker = { enabled = false },
 })
 
