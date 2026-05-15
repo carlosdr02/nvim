bade959418e9d4621434468d6393e79f6d56a4fb
Carlos Delgado Ramos <carlosdelgadoramos2002@gmail.com>
2026-05-15 11:46:25 +0200

    new one

--- init.lua
+++ init.lua
@@ -1,67 +1,72 @@
--- SETTINGS
-vim.opt.signcolumn = 'no'
-vim.opt.guicursor = ''
+vim.g.mapleader = ' '
+
+vim.opt.guicursor = ""
+vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 12
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.opt.splitbelow = true
+vim.opt.showmode = false
+vim.opt.swapfile = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'no'
 
--- PLUGINS
 vim.pack.add({
-    -- deps
-    { src = 'https://github.com/rafamadriz/friendly-snippets' },
-    { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
-    { src = 'https://github.com/nvim-lua/plenary.nvim' },
-
     -- colorschemes
-    { src = 'https://github.com/rebelot/kanagawa.nvim' },
     { src = 'https://github.com/ellisonleao/gruvbox.nvim' },
-    { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
 
-    -- lsp config and mason
-    { src = 'https://github.com/mason-org/mason.nvim' },
+    -- oil.nvim
+    { src = 'https://github.com/nvim-mini/mini.icons' },
+    { src = 'https://github.com/stevearc/oil.nvim' },
+
+    -- nvim-autopairs
+    { src = 'https://github.com/windwp/nvim-autopairs' },
+
+    -- lualine.nvim
+    { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+    { src = 'https://github.com/nvim-lualine/lualine.nvim' },
+
+    -- vim-fugitive
+    { src = 'https://github.com/tpope/vim-fugitive' },
+
+    -- harpoon
+    { src = 'https://github.com/nvim-lua/plenary.nvim' },
+    { src = 'https://github.com/theprimeagen/harpoon' },
+
+    -- fzf-lua
+    { src = 'https://github.com/ibhagwan/fzf-lua' },
+    { src = 'https://github.com/nvim-tree/nvim-web-devicons' },
+
+    -- lsp & mason
     { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/mason-org/mason.nvim' },
     { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
 
-    -- others
-    { src = 'https://github.com/nvim-mini/mini.nvim', version = 'stable' },
-    { src = 'https://github.com/stevearc/oil.nvim' },
+    -- blink.cmp
     { src = 'https://github.com/saghen/blink.cmp', version = 'v1' },
-    { src = 'https://github.com/ibhagwan/fzf-lua' },
-    { src = 'https://github.com/nvim-lualine/lualine.nvim' },
-    { src = 'https://github.com/tpope/vim-fugitive' },
-    { src = 'https://github.com/theprimeagen/harpoon' },
-    { src = 'https://github.com/windwp/nvim-autopairs' },
+
+    -- mini.nvim
+    { src = 'https://github.com/nvim-mini/mini.move' },
+    { src = 'https://github.com/nvim-mini/mini.basics' },
 })
 
--- PLUGINS CONFIG
-require('mason').setup()
-require('mason-lspconfig').setup {
-    ensure_installed = { 'clangd', 'ts_ls', 'pyright' },
-}
+vim.cmd.colorscheme('gruvbox')
+vim.diagnostic.config({ virtual_text = true })
 
-
-require('mini.move').setup()
-require('mini.comment').setup()
-require('mini.icons').setup()
 require('mini.basics').setup {
+    options = {
+        basic = false,
+    },
     mappings = {
-        windows = true
-    }
-}
-
-
-local fzf = require('fzf-lua')
-fzf.setup {
-    fzf_opts   = {
-        ["--cycle"] = true
-    },
-    builtin = { -- TODO: see if there's a way to use C-d and C-u
-        ["<C-j>"] = "preview-page-down",
-        ["<C-k>"] = "preview-page-up"
+        windows = true,
+        move_with_alt = true,
     },
 }
-
-
+require('mini.icons').setup()
+require('mini.move').setup()
+require('oil').setup()
+require('nvim-autopairs').setup()
 require('lualine').setup {
     sections = {
         lualine_c = {
@@ -72,24 +77,43 @@ require('lualine').setup {
         }
     }
 }
-
-
+local fzf = require('fzf-lua')
+fzf.setup {
+    fzf_opts   = {
+        ["--cycle"] = true
+    },
+    builtin = { -- TODO: see if there's a way to use C-d and C-u
+        ["<C-j>"] = "preview-page-down",
+        ["<C-k>"] = "preview-page-up"
+    },
+}
+require("mason").setup()
+require("mason-lspconfig").setup {
+    ensure_installed = { "clangd", "pyright", "ts_ls" },
+}
 require('blink.cmp').setup {
     signature = { enabled = true }
 }
 
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
-require('oil').setup()
-require("nvim-autopairs").setup()
+-- harpoon keymaps
+local hmark = require('harpoon.mark')
+local hui = require('harpoon.ui')
+vim.keymap.set("n", "<leader>ha", hmark.add_file, { desc = "Add file to harpoon" })
+vim.keymap.set("n", "<leader>hm", hui.toggle_quick_menu, { desc = "Toggle harpoon menu" })
+vim.keymap.set("n", "<leader>1", function() hui.nav_file(1) end, { desc = "Go to harpoon file 1" })
+vim.keymap.set("n", "<leader>2", function() hui.nav_file(2) end, { desc = "Go to harpoon file 2" })
+vim.keymap.set("n", "<leader>3", function() hui.nav_file(3) end, { desc = "Go to harpoon file 3" })
+vim.keymap.set("n", "<leader>4", function() hui.nav_file(4) end, { desc = "Go to harpoon file 4" })
+vim.keymap.set("n", "<leader>5", function() hui.nav_file(5) end, { desc = "Go to harpoon file 5" })
+vim.keymap.set("n", "<leader>6", function() hui.nav_file(6) end, { desc = "Go to harpoon file 6" })
+vim.keymap.set("n", "<leader>7", function() hui.nav_file(7) end, { desc = "Go to harpoon file 7" })
+vim.keymap.set("n", "<leader>8", function() hui.nav_file(8) end, { desc = "Go to harpoon file 8" })
+vim.keymap.set("n", "<leader>9", function() hui.nav_file(9) end, { desc = "Go to harpoon file 9" })
+vim.keymap.set("n", "<leader>0", function() hui.nav_file(10) end, { desc = "Go to harpoon file 10" })
 
--- COLORSCHEME
-vim.cmd.colorscheme('gruvbox')
-vim.diagnostic.config({ virtual_text = true })
-
--- KEYMAPS
-vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-
-
+-- fzf-lua keymaps
 vim.keymap.set('n', '<leader>ff', fzf.files, { desc = 'Find files' })
 vim.keymap.set('n', '<leader>fb', fzf.buffers, { desc = 'Find open buffers' })
 vim.keymap.set('n', '<leader>fg', fzf.live_grep_native, { desc = 'Live grep' })
@@ -123,23 +147,7 @@ vim.keymap.set('n', '<leader>mcs', fzf.colorschemes, { desc = 'Misc colorschemes
 vim.keymap.set('n', '<leader>mcc', fzf.commands, { desc = 'Misc neovim commands' })
 vim.keymap.set('n', '<leader>mch', fzf.command_history, { desc = 'Misc command history' })
 
-
-local hmark = require('harpoon.mark')
-local hui = require('harpoon.ui')
-vim.keymap.set("n", "<leader>ha", hmark.add_file, { desc = "Add file to harpoon" })
-vim.keymap.set("n", "<leader>hm", hui.toggle_quick_menu, { desc = "Toggle harpoon menu" })
-vim.keymap.set("n", "<leader>1", function() hui.nav_file(1) end, { desc = "Go to harpoon file 1" })
-vim.keymap.set("n", "<leader>2", function() hui.nav_file(2) end, { desc = "Go to harpoon file 2" })
-vim.keymap.set("n", "<leader>3", function() hui.nav_file(3) end, { desc = "Go to harpoon file 3" })
-vim.keymap.set("n", "<leader>4", function() hui.nav_file(4) end, { desc = "Go to harpoon file 4" })
-vim.keymap.set("n", "<leader>5", function() hui.nav_file(5) end, { desc = "Go to harpoon file 5" })
-vim.keymap.set("n", "<leader>6", function() hui.nav_file(6) end, { desc = "Go to harpoon file 6" })
-vim.keymap.set("n", "<leader>7", function() hui.nav_file(7) end, { desc = "Go to harpoon file 7" })
-vim.keymap.set("n", "<leader>8", function() hui.nav_file(8) end, { desc = "Go to harpoon file 8" })
-vim.keymap.set("n", "<leader>9", function() hui.nav_file(9) end, { desc = "Go to harpoon file 9" })
-vim.keymap.set("n", "<leader>0", function() hui.nav_file(10) end, { desc = "Go to harpoon file 10" })
-
-
+-- some lsp keymaps
 vim.keymap.set('n', '<c-n>', function() vim.diagnostic.jump({ count = 1 }) end, { desc = "Go to next diagnostic" })
 vim.keymap.set('n', '<c-p>', function() vim.diagnostic.jump({ count = -1 }) end, { desc = "Go to previous diagnostic" })
 vim.keymap.set('n', '<leader>j', vim.diagnostic.open_float, { desc = "Open diagnostic window" })
